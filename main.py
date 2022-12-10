0fa14d01b646891472d529753bf14de2252d8357
marat <maratgalyavov@gmail.com>
2022-12-10 20:24:14 +0300

    и

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import streamlit as st
 from Funct import draw
 from Funct import tools
 
-st.markdown("# Main project page️")
+st.markdown("# Main page️")
 
 df = pd.read_csv("Data/HR Employee Attrition.csv")
 df2 = pd.read_csv("Data/salary_data_cleaned.csv")
@@ -14,7 +14,6 @@ df3 = pd.read_csv("Data/unemployment analysis.csv")
 
 df, df2, df3 = tools.cleanup(df, df2, df3)
 df = tools.modify(df)
-levels_sat = sorted(list(df["JobSatisfaction"].unique()))
 df = df.sort_values(by=["JobSatisfaction", "MonthlyIncome", "JobInvolvement"])
 df2 = df2.sort_values(by=["age", "avg_salary"])
 
@@ -24,6 +23,8 @@ with tab1:
     st.header("Employee satisfaction")
     st.write(
         "I assume that monthly income and job involvement have a healthy effect on job satisfaction, to prove it, I plotted bar charts for each of these parameters in general and for different professions. Charts you see below are a great proof for my hypothesis, as they show that income has a very similar effect on one's level of satisfaction as job involvement, where individuals with highest income and/or involvement tend to be more satisfied with their career")
+    st.write(
+        "Bar chaart was chosen to represent this because it is easy to undersrtand and very easy to compare parameters")
     option = st.selectbox("profession", (["All"] + list(df["JobRole"].unique())))
     if option == "All":
         tmp = df
@@ -34,6 +35,7 @@ with tab1:
     st.header("Job satisfaction by monthly salary")
     st.write(
         "On the pie chart below, i represented different percentages of job satisfaction based on salary, the chart is interactive so you can select desired level of income and view percentages of different satisfaction levels among individuals whose income is within +-500$ of the selected amount")
+    st.write("i chose pie chart here because it is the best plot to depict percentages and ratios")
     number = st.slider("Salary", 1000, 20000)
     df = df[(df["MonthlyIncome"] >= number - 500) & (df["MonthlyIncome"] <= number + 500)]
     draw.drawpie(df, "JobSatisfaction")
@@ -41,7 +43,9 @@ with tab1:
 with tab2:
     st.header("Wage")
     st.write(
-        "Bow chart below shows minimum and average wage requirements for individuals based on their age. Box chart was chosen here to make average values more clear as well as show extremes. I want to prove that most people get paid above their minimum threshold and this gap is not dependant on one's age")
+        "Box chart below shows minimum and average wage requirements for individuals based on their age. Box chart was chosen here to make average values more clear as well as show extremes. I want to prove that most people get paid above their minimum threshold and this gap is not dependant on one's age")
+    st.write(
+        "box charts are great to present gradial statistical data and it's deviations, while simultaniously looking cool")
     draw.drawbox(df2, "age", "min_salary", "Age", "Minimal Salary")
     draw.drawbox(df2, "age", "avg_salary", "Age", "Average Salary")
     st.write(
@@ -51,6 +55,7 @@ with tab3:
     st.header("Unemployment")
     st.write(
         "Level of unemployment generally reflects what is happening with the world economy. line chart below depicts unemployment percentage in europe, us and average among all countries. all three trends follow the same pattern, proving the point that level of uneployment is a great metric for analyzing worldwide economical situation ")
+    st.write("")
     years = list(map(str, (range(1991, 2022))))
     medians = []
     europe = []
